d721baa0309e652dc634ed3aaa7750a1f64b8742
zhangkeqiang <zhangkeqiang@tsjinrong.cn>
2015-10-19 21:43:57 +0800

    optimize the Log2j2

--- src/test/java/kata/Log2j2Test.java
+++ src/test/java/kata/Log2j2Test.java
@@ -3,31 +3,56 @@ package kata;
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log2j2Test {
-
-	@Before
-	public void setUp() throws Exception {
+	static Logger logger;
+	static int i = 0;
+	static int j = 0;
+	@Before 
+	public void init() {
+		logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+		logger.info("init time:" + (++i));
 	}
-
 	@After
 	public void tearDown() throws Exception {
+		logger.info("after time:" + i);
 	}
 
+	@BeforeClass
+	public static void beforeClass(){
+		LogManager.getLogger(LogManager.ROOT_LOGGER_NAME).info("beforeclass time:" + (++j));
+	}
+	
+	@AfterClass
+	public static void afterClass(){
+		LogManager.getLogger(LogManager.ROOT_LOGGER_NAME).info("afterclass time:" + (j));
+	}
 	@Test
 	public void testLog4j2() {
-		Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+		assertNotNull(logger);
         logger.trace("trace message");
         logger.debug("debug message");
         logger.info("info message");
         logger.warn("warn message");
         logger.error("error message");
         logger.fatal("fatal message");
-        System.out.println("Hello World!");
-
+	}
+	
+	
+	@Test
+	public void testLog4j2debug() {		
+        logger.debug("debug message");
+        logger.debug("debug message2");
+	}
+	
+	@Test
+	public void testLog4j2fatal() {		
+        logger.fatal("fatal message");
 	}
 
 }

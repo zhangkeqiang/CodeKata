8daa80667dc64a8a718ace8597b1f5bd076cf515
zhangkeqiang <zhangkeqiang@tsjinrong.cn>
2015-11-19 16:45:25 +0800

    add IncomeTaxTest2

---
+++ src/test/java/kata/IncomeTaxTest2.java
@@ -0,0 +1,52 @@
+package kata;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class IncomeTaxTest2{
+	private IncomeTax tax;
+	private Object parameters[][] = {
+			{100,0},
+			{3500, 0},
+			{4000, 15},
+			{6000, 15},
+			{10000, 15},
+	};
+	protected void setUp() throws Exception {
+		tax = new IncomeTax();
+	}
+	
+	public void testTax(){
+		assert.assertArrayEquals(expecteds, actuals);  assertEquals(0.0, tax.calculate(100));
+	}
+	
+	public void test3500(){
+		assertEquals(0.0, tax.calculate(3500));
+	}
+	
+	public void test4000(){
+		assertEquals(15.0, tax.calculate(4000));
+	}
+	
+	public void test6000(){
+		assertEquals(145.0, tax.calculate(6000));
+	}
+	
+	public void test10000(){
+		assertEquals(745.0, tax.calculate(10000));
+	}
+	
+	public void test20000(){
+		assertEquals(3120.0, tax.calculate(20000));
+	}
+	
+	public void test50000(){
+		assertEquals(18120.0, tax.calculate(50000));
+	}
+	
+/**/
+}

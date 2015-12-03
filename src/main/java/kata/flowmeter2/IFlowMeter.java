5cce31ad01c50cddb0e104c7ab1746647089d806
zhangkeqiang <zhangkeqiang@tsjinrong.cn>
2015-12-03 18:07:05 +0800

    add sonarlint and fix some sonar bugs

--- src/main/java/kata/flowmeter2/IFlowMeter.java
+++ src/main/java/kata/flowmeter2/IFlowMeter.java
@@ -1,5 +1,5 @@
 package kata.flowmeter2;
 
 public interface IFlowMeter {
-	public int calculateBillingFlow(PlanUser user, int year, int month);
+    public int calculateBillingFlow(PlanUser user, int year, int month);
 }

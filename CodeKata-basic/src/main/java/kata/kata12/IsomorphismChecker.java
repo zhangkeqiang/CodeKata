dead1c5658b46998235c038be7192df2b95eeb63
Mike Zhang <zhangkeqiang@gmail.com>
2019-12-14 21:31:48 +0800

    add 1st test

---
+++ CodeKata-basic/src/main/java/kata/kata12/IsomorphismChecker.java
@@ -0,0 +1,9 @@
+package kata.kata12;
+
+public class IsomorphismChecker {
+
+	public static boolean foo(String S, String T) {
+		return true;
+	}
+
+}
---
+++ CodeKata-basic/src/test/java/kata/kata12/IsomorphismCheckerTest.java
@@ -0,0 +1,24 @@
+package kata.kata12;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IsomorphismCheckerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		assertTrue(IsomorphismChecker.foo("a","b"));
+	}
+
+}

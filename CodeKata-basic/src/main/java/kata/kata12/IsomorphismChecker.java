86522a8b22ce79d62cd928fb3dd98be0230cf1f1
Mike Zhang <zhangkeqiang@gmail.com>
2019-12-15 19:31:32 +0800

    refine format

--- CodeKata-basic/src/main/java/kata/kata12/IsomorphismChecker.java
+++ CodeKata-basic/src/main/java/kata/kata12/IsomorphismChecker.java
@@ -1,29 +1,13 @@
 package kata.kata12;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class IsomorphismChecker {
-	
-	static protected Logger log = LogManager.getLogger();
-	
-	public static boolean foo(String S, String T) {
-		if(S == null | T== null) return false;
-		if(S.length() != T.length()) return false;
-		StringBuffer SS = new StringBuffer("A");
-		StringBuffer TT = new StringBuffer("A");
-		int j = (int)'A';
-		for(int i=1;i<S.length();i++) {
-			int sj = S.indexOf(S.charAt(i));
-			SS.append((char)(j+sj));
-			int tj = T.indexOf(T.charAt(i));
-			TT.append((char)(j+tj));
-			log.debug(SS);
-			log.debug(TT);
-			if(!SS.toString().equals(TT.toString()))
+	public static boolean foo(String s, String t) {
+		if (s == null || t == null || s.length() != t.length())
+			return false;
+		for (int i = 1; i < s.length(); i++) {
+			if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i)))
 				return false;
 		}
 		return true;
 	}
-
 }
--- CodeKata-basic/src/test/java/kata/kata12/IsomorphismCheckerTest.java
+++ CodeKata-basic/src/test/java/kata/kata12/IsomorphismCheckerTest.java
@@ -7,41 +7,41 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IsomorphismCheckerTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public final void testab() {
-		assertTrue(IsomorphismChecker.foo("a","b"));
+		assertTrue(IsomorphismChecker.foo("a", "b"));
 	}
 
 	@Test
 	public final void testaabb() {
-		assertTrue(IsomorphismChecker.foo("aa","bb"));
-	}
-	
-	@Test
-	public final void testlength() {
-		assertFalse(IsomorphismChecker.foo("aaa","bb"));
-	}
-	
-	@Test
-	public final void testababab() {
-		assertTrue(IsomorphismChecker.foo("abab","baba"));
-		assertTrue(IsomorphismChecker.foo("abadddaaa1234","babcccbbb5678"));
-		assertTrue(IsomorphismChecker.foo("abadddaaazzzzzz","babcccbbbeeeeee"));
-	}
-	
-	@Test
-	public final void testabababb() {
-		assertFalse(IsomorphismChecker.foo("abaddddaaa","babccczbbb"));
+		assertTrue(IsomorphismChecker.foo("aa", "bb"));
 	}
 
-	
+	@Test
+	public final void testlength() {
+		assertFalse(IsomorphismChecker.foo("aaa", "bb"));
+	}
+
+	@Test
+	public final void testababab() {
+		assertTrue(IsomorphismChecker.foo("egg", "add"));
+		assertFalse(IsomorphismChecker.foo("foo", "bar"));
+	}
+
+	@Test
+	public final void testabababb() {
+		assertFalse(IsomorphismChecker.foo("abaddddaaa", "babccczbbb"));
+		assertFalse(IsomorphismChecker.foo("abadddaaad234", "babcccbbb5678"));
+		assertFalse(IsomorphismChecker.foo("abadddaaazzzzzc", "babcccbbbeeeeee"));
+		assertFalse(IsomorphismChecker.foo("asdfghjka", "bwertyuwb"));
+	}
+
+	@Test
+	public final void testababab_more() {
+		assertTrue(IsomorphismChecker.foo("abab", "baba"));
+		assertTrue(IsomorphismChecker.foo("abadddaaa1234", "babcccbbb5678"));
+		assertTrue(IsomorphismChecker.foo("abadddaaazzzzzz", "babcccbbbeeeeee"));
+		assertTrue(IsomorphismChecker.foo("asdfghjka", "bwertyuib"));
+	}
+
 }

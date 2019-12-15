8ef2d1b3ea528fd2dbdc36e9e643e15fbff22c65
Mike Zhang <zhangkeqiang@gmail.com>
2019-12-15 22:03:23 +0800

    get the result

--- CodeKata-basic/src/main/java/kata/Checker.java
+++ CodeKata-basic/src/main/java/kata/Checker.java
@@ -1,29 +1,14 @@
 package kata;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Checker {
-	protected static Logger log = LogManager.getLogger();
 
 	public static boolean checkIsomorphic(String s, String t) {
-		if (s == null || t == null)
-			return false;
-		if (s.length() != t.length())
-			return false;
-		if (s.length() == 1)
-			return true;
-		StringBuffer sf = new StringBuffer("A");
-		StringBuffer tf = new StringBuffer("A");
-		for (int i = 1; i < s.length(); i++) {
-			sf.append((char) ('A' + s.indexOf(s.charAt(i))));
-			tf.append((char) ('A' + t.indexOf(t.charAt(i))));
-			log.debug(sf.toString());
-			log.debug(tf.toString());
-			if (!sf.toString().equals(tf.toString()))
+		for(int i=1; i<s.length();i++) {
+			int si = s.indexOf(s.charAt(i));
+			int ti= t.indexOf(t.charAt(i));
+			if(si != ti)
 				return false;
 		}
 		return true;
-
 	}
 }

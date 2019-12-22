41eeb71293de6b2eb05978af1c3bb65e4b5aa533
Mike Zhang <zhangkeqiang@gmail.com>
2019-12-22 11:41:53 +0800

    add FizzBuzz

---
+++ CodeKata-basic/src/main/java/kata/kata12/FizzBuzz.java
@@ -0,0 +1,32 @@
+package kata.kata12;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FizzBuzz {
+	protected static Logger logger = LogManager.getLogger();
+	
+	public static void main(String[] args) {
+		FizzBuzz fiz = new FizzBuzz();
+		for (int i = 1; i < 101; i++) {
+			logger.info(fiz.getFizzBuzzValue(i));
+		}
+	}
+	
+	public String getFizzBuzzValue(int number) {
+		if (isMultipleByParam(number, 15)) {
+			return "FizzBuzz";
+		} else if (isMultipleByParam(number, 3)) {
+			return "Fizz";
+		} else if (isMultipleByParam(number, 5)) {
+			return "Buzz";
+		} else {
+			return Integer.toString(number);
+		}
+	}
+
+	protected boolean isMultipleByParam(int number, int j) {
+		return number % j == 0;
+	}
+
+}

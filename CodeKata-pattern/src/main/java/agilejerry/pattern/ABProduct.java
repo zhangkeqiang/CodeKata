2ea6185f8cbd7735195b71c6241716e284b7ed5f
Mike Zhang <zhangkeqiang@gmail.com>
2019-12-24 22:46:53 +0800

    fix old critical Sonarqube NC

--- CodeKata-pattern/src/main/java/agilejerry/pattern/ABProduct.java
+++ CodeKata-pattern/src/main/java/agilejerry/pattern/ABProduct.java
@@ -1,11 +1,13 @@
 package agilejerry.pattern;
 
+import java.util.logging.Logger;
+
 public class ABProduct extends ProductSkeleton {
 
+	@Override
 	public void fly() {
-		
+		Logger.getAnonymousLogger().info("fly to sky on AB");
 	}
-
 
 	@Override
 	protected int step1() {
@@ -15,8 +17,7 @@ public class ABProduct extends ProductSkeleton {
 	@Override
 	protected int step2(int i) {
 		return 123;
-		
+
 	}
-	
 
 }

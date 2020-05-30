1e40331da9177c310ab7c3433587fb563fb73383
Mike Zhang <zhangkeqiang@gmail.com>
2020-05-31 07:53:49 +0800

    remove testSetGetYard2

--- CodeKata-basic/src/test/java/kata/kata12/RulerTest.java
+++ CodeKata-basic/src/test/java/kata/kata12/RulerTest.java
@@ -46,12 +46,4 @@ public class RulerTest {
 		assertEquals(3.5, ruler.getYard(), SMALLDELTA);
 	}
 
-	@Test
-	public void testSetGetYard2() {
-		ruler.setYard(4.5);
-		assertEquals(13.5, ruler.getFeet(), SMALLDELTA);
-		assertEquals(162, ruler.getInch(), SMALLDELTA);
-		assertEquals(4.5, ruler.getYard(), SMALLDELTA);
-	}
-
 }

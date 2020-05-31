a69a221502cf733f1d346b5937a792a02d303640
Mike Zhang <zhangkeqiang@gmail.com>
2020-05-31 15:37:05 +0800

    remove system.out from AACommand

--- CodeKata-pattern/src/main/java/agilejerry/pattern/AACommand.java
+++ CodeKata-pattern/src/main/java/agilejerry/pattern/AACommand.java
@@ -1,7 +1,10 @@
 package agilejerry.pattern;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AACommand implements Command {
 	
+	Logger logger = LogManager.getLogger();  //LogManager.ROOT_LOGGER_NAME
 	private int II;
 	public AACommand(int i){
 		this.II = i;
@@ -11,7 +14,7 @@ public class AACommand implements Command {
 		this.II = 10;
 	}
 	public boolean execute() {
-		System.out.println("AACommand"  + II);
+		logger.info("AACommand"  + II);
 		return true;
 	}
 

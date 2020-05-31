9d6c92dfb22fe056583f47ba539d3ae59388d56f
Mike Zhang <zhangkeqiang@gmail.com>
2020-05-31 21:20:32 +0800

    repair App and AppTest

--- CodeKata-pattern/src/main/java/agilejerry/pattern/App.java
+++ CodeKata-pattern/src/main/java/agilejerry/pattern/App.java
@@ -1,13 +1,21 @@
 package agilejerry.pattern;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static Logger logger = LogManager.getLogger(); 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	processMain();
+    }
+    
+    static void processMain() {
+    	 logger.info( "Hello World!" );
     }
 }

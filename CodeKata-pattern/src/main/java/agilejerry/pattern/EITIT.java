866bfbd04af5992c234efdce3c7e07f7a88eb9ca
Mike Zhang <zhangkeqiang@gmail.com>
2020-06-06 10:39:08 +0800

    fix smell 1

--- CodeKata-pattern/src/main/java/agilejerry/pattern/EITIT.java
+++ CodeKata-pattern/src/main/java/agilejerry/pattern/EITIT.java
@@ -1,9 +1,13 @@
 package agilejerry.pattern;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class EITIT implements EITI {
 
+	protected Logger logger = LogManager.getLogger();
 	public int run() {
-		System.out.println("TIEIT.run");
+		logger.info("TIEIT.run");
 		return 0;
 		
 	}

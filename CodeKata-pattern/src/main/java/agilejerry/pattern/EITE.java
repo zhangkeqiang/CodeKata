eda020e2210e6aca13118f49e6da878e7b06588e
Mike Zhang <zhangkeqiang@gmail.com>
2020-06-07 11:34:33 +0800

    fix Reorder the modifiers to comply with the Java Language pecification.

--- CodeKata-pattern/src/main/java/agilejerry/pattern/EITE.java
+++ CodeKata-pattern/src/main/java/agilejerry/pattern/EITE.java
@@ -3,21 +3,24 @@ package agilejerry.pattern;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public abstract class EITE implements EITI{
+public abstract class EITE implements EITI {
 	protected EITContext context;
 	protected Logger logger = LogManager.getLogger();
-	public int run(){
+
+	public int run() {
 		logger.debug("EITE.run...run1 = " + run1());
 		return run1() + run2();
 	}
-	
+
 	protected abstract int run1();
-	abstract protected int run2();
+
+	protected abstract int run2();
 
 	public void setContext(EITContext theContext) {
 		this.context = theContext;
 	}
-	public void setContextI(int i){
+
+	public void setContextI(int i) {
 		context.setI(i);
 	}
 }

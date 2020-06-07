b6fbf73d0be86ab8a17b5b339ff8429d4d653181
Mike Zhang <zhangkeqiang@gmail.com>
2020-06-07 22:32:31 +0800

    test ABProduct by powermock

--- CodeKata-pattern/src/test/java/agilejerry/pattern/FactoryTest.java
+++ CodeKata-pattern/src/test/java/agilejerry/pattern/FactoryTest.java
@@ -3,16 +3,32 @@ package agilejerry.pattern;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import static org.powermock.api.mockito.PowerMockito.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Settings.class)
 public class FactoryTest {
 
 	@Test
-	public void testCreateProduct() {
-		//SimpleFactory factory = new SimpleFactory();
-		//assertNotNull(factory);
+	public void testCreateAAProduct() {
 		Product product = SimpleFactory.createProduct();
 		assertNotNull(product);
-		
+		assertTrue(product instanceof AAProduct);
 	}
+	
+	
+	@Test
+	public void testCreateABProduct() throws Exception{
+		mockStatic(Settings.class);
+		when(Settings.getProduct()).thenReturn("AB");
+		Product product = SimpleFactory.createProduct();
+		assertNotNull(product);
+		assertTrue(product instanceof ABProduct);
+		verifyStatic();
+	}
+	
 
 }

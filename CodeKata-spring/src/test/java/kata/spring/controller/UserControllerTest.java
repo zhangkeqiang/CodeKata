e1f8eac212fa67cda18bc3d6e5a099f31a1b8bf8
Mike Zhang <zhangkeqiang@gmail.com>
2020-06-07 20:13:12 +0800

    remove block

--- CodeKata-spring/src/test/java/kata/spring/controller/UserControllerTest.java
+++ CodeKata-spring/src/test/java/kata/spring/controller/UserControllerTest.java
@@ -9,39 +9,48 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.web.servlet.ModelAndView;
 
-/**@ClassName: UserControllerTest
- * @Description:  
+/**
+ * @ClassName: UserControllerTest
+ * @Description:
  */
 public class UserControllerTest {
 
-    private UserController userController;  
-    /**@method setUp
-     * @description 
-     * @return void
-    */
-    //@Before
-    public void setUp() throws Exception {
-        userController = new UserController();  
-    }
+	private UserController userController;
 
-    /**
-     * Test method for {@link kata.spring.controller.UserController#view(java.lang.Long, javax.servlet.http.HttpServletRequest)}.
+	/**
+	 * @method setUp
+	 * @description
+	 * @return void
+	 */
+	// @Before
+	public void setUp() throws Exception {
+		userController = new UserController();
+	}
 
-    @Test
-    @Ignore
-    */
-    public final void testView() {
-    	assertNotNull(userController);
-        MockHttpServletRequest req = new MockHttpServletRequest();  
-        assertNotNull(req);
-        String localName = "localName";
+	@Test
+	public void helloTest() {
+		assertNull(userController);
+	}
+
+	/**
+	 * Test method for
+	 * {@link kata.spring.controller.UserController#view(java.lang.Long, javax.servlet.http.HttpServletRequest)}.
+	 * 
+	 * @Test
+	 * @Ignore
+	 */
+	public final void testView() {
+		assertNotNull(userController);
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		assertNotNull(req);
+		String localName = "localName";
 		req.setLocalName(localName);
-        assertEquals(localName,req.getLocalName());
+		assertEquals(localName, req.getLocalName());
 
-//        ModelAndView mv = userController.view(14544L, req);  
-//  
-//        ModelAndViewAssert.assertViewName(mv, "user/view");  
-//        ModelAndViewAssert.assertModelAttributeAvailable(mv, "user");  
-    }
+		// ModelAndView mv = userController.view(14544L, req);
+		//
+		// ModelAndViewAssert.assertViewName(mv, "user/view");
+		// ModelAndViewAssert.assertModelAttributeAvailable(mv, "user");
+	}
 
 }

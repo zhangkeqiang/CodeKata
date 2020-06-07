4f9545fe31481149dcfc460925ff121b86388a81
Mike Zhang <zhangkeqiang@gmail.com>
2020-06-07 08:18:52 +0800

    add more test data by arraylist

--- CodeKata-basic/src/test/java/kata1/MaskifyTest.java
+++ CodeKata-basic/src/test/java/kata1/MaskifyTest.java
@@ -2,20 +2,36 @@ package kata1;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class MaskifyTest {
 	Maskify mask = new Maskify();
+	ArrayList<String[]> alTestData = new ArrayList<String[]>();
+
+	@Before
+	public void setUp(){
+		String[] testdata1 = { "abddssss", "####ssss" };
+		String[] testdata2 = { "abdd", "abdd" };
+		String[] testdata3 = { "ab", "ab" };
+		String[] testdata4 = { "DOS", "DOS" };
+		String[] testdata5 = { "1234567890DOSD", "##########DOSD" };
+		String[] testdata6 = { "1234567890____DOSD", "##############DOSD" };
+
+		alTestData.add(testdata1);
+		alTestData.add(testdata2);
+		alTestData.add(testdata3);
+		alTestData.add(testdata4);
+		alTestData.add(testdata5);
+		alTestData.add(testdata6);
+	}
 
 	@Test
 	public final void testHideLast4Chars() {
-		assertEquals("####SDDS", mask.hideLast4Chars("abcdSDDS"));
+		for (String[] testdata : alTestData) {
+			assertEquals(testdata[1], mask.hideLast4Chars(testdata[0]));
+		}
 	}
-	
-	@Test
-	public final void testHideLast4CharsforShortString() {
-		
-		assertEquals("DDS", mask.hideLast4Chars("DDS"));
-	}
-
 }
